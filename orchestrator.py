1e2e259f4273d3636ac16e3dd557e4d9193d99a8
Mangel1782 <miguelangelbarros@gmail.com>
2022-01-22 19:03:13 -0500

    meanor tweakes

--- orchestrator.py
+++ orchestrator.py
@@ -4,12 +4,14 @@ from classifier import runClassifier
 from global_report import overallReport
 import os
 
+
 if __name__ == "__main__":
 
     os.mkdir("genomes_dir")
     main_dir = os.getcwd() + "/genomes_dir/"
     genome_bank_file = os.getcwd() + "/prokaryotes_12_20_2020.csv"
-    path_to_model = os.getcwd() + "/estimator_p.sav"
+    path_to_model = os.getcwd() +  "/estimator_v0122.sav"
+
 
     GS = genomeScraper()
     PG = processGenomes()
@@ -18,14 +20,15 @@ if __name__ == "__main__":
 
     scrapper = GS.orchestrator(file_name=genome_bank_file,
 							   target_directory=main_dir)
+
     process_genomes = PG.orchestrator(main_dir=main_dir,
-                                  save_dataframe=True,
-                                  create_summary=True)
+                                      save_dataframe=True,
+                                      create_summary=True)
 
     IS_classifier = ALG.executor(main_dir=main_dir,
-                                 path_to_model = path_to_model,
+                                 path_to_model=path_to_model,
                                  accuracy=True)
 
-    OR = overallReport.compute_summary(path_to_data = main_dir)
+    OR = overallReport.compute_summary(path_to_data=main_dir)
 
 
